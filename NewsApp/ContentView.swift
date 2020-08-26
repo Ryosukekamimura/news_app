a06f9eb8a4ca4e5d6aeeaaa623fd9f4ce8d20c95
RyosukeKamimura <come1234560@gmail.com>
2020-08-26 12:19:40 +0900

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by 神村亮佑 on 2020/08/26.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
